d4c2cdeac3817c468c4bdea15ebb0438e0c3adf4
Krittaphas Kunkhongkaphan <tarm.kun@gmail.com>
2024-12-10 18:49:37 +0700

    asdf

--- app/score/[id]/page.tsx
+++ app/score/[id]/page.tsx
@@ -1,9 +1,15 @@
-import ScoreDetailsClient from './ScoreDetailsClient'
+import { Suspense } from 'react'
+import ScoreDetailsPage from './ScoreDetailsPage'
 
 interface PageProps {
   params: { id: string }
 }
 
-export default function ScoreDetailsPage({ params }: PageProps) {
-  return <ScoreDetailsClient id={params.id} />
+export default function Page({ params }: PageProps) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ScoreDetailsPage id={params.id} />
+    </Suspense>
+  )
 }
+
