c24d1633f36df4f5742572d8d7547fc1b6b182b4
Krittaphas Kunkhongkaphan <tarm.kun@gmail.com>
2024-12-21 15:50:01 +0700

    Refactor Next.js configuration: integrate internationalization support, remove unused community page, and optimize build manifests. Update image settings and streamline webpack runtime for improved performance.

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'th'],
   },
 }
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/score/:id',
-        destination: '/score/[id]',
-      },
-    ]
-  },
-}
 module.exports = nextConfig
-
-const { i18n } = require('./next-i18next.config')
-
-module.exports = {
-  i18n,
-}
