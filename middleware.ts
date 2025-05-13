a6227f773fa9babaada025000c8f6623ccbcee7c
Krittaphas Kunkhongkaphan <tarm.kun@gmail.com>
2025-05-14 01:24:34 +0700

    Remove middleware config export

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,3 @@ export function middleware(request: NextRequest) {
   // Simple pass-through middleware
   return NextResponse.next()
 }
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
-}
